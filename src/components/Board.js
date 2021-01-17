bfdd29cb9faaf3c18dd07a31749ad1f20cc5d96e
Kareha Agesa <kmaagesa@gmail.com>
2021-01-16 18:10:59 -0800

    clean up comments

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,62 +5,16 @@ import axios from 'axios';
 import './Board.css';
 import Card from './Card';
 import NewCardForm from './NewCardForm';
-// import CARD_DATA from '../data/card-data.json';
-
-// const generateCards = CARD_DATA.cards.map((card) => {
-//   if (card.Emoji) {
-//     return(
-//       <Card text={card.text} emojiString={card.Emoji} />
-//     )
-//   }
-//   return(
-//     <Card text={card.text} />
-//   )
-
-// })
-
 
 const listCards = (cards) => {
 
     return (cards.map((cardHolder) => {
-      if (cardHolder.card['emoji']){
-        console.log(cardHolder.card['text'])
         return(
           <Card text={cardHolder.card['text']} emojiString={cardHolder.card['emoji']} key={cardHolder.card['id']} />
         )
-      } else {
-        return(
-          <Card text={cardHolder.card['text']} key={cardHolder.card['id']} />
-        )
-      }
     }))
-
-    // response.map((card) => {
-    //   if (card.Emoji) {
-    //     return(
-    //       <Card text={card.text} emojiString={card.Emoji} />
-    //     )
-    //   }
-    //   return(
-    //     <Card text={card.text} />
-    //   )
-    // })
-
 };
 
-// const printCards = (cards) => {
-//   cards.map((cardHolder) => {
-//     if (cardHolder.card['Emoji']){
-//       // console.log(cardHolder.card['text'])
-//       return(
-//         <Card text={cardHolder.card['text']} emojiString={cardHolder.card['emoji']} />
-//       )
-//     } else {
-//       return(
-//         <Card text={cardHolder.card['text']} />
-//       )
-//     }
-// }
 
 const Board = ({url, boardName}) => {
 
@@ -74,13 +28,12 @@ const Board = ({url, boardName}) => {
 
     })
     .catch((error) => {
-
+      console.log(error.message)
     })
   }, [url, boardName]);
 
   return (
     <div>
-      {console.log(cards)}
       {listCards(cards)}    
     </div>
   )
